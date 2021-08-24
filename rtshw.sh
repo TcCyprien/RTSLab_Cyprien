734b49da13f174d3f1c995a372edbf99fe39ac7f
cyprien <tchatchou77@yahoo.fr>
2021-08-23 23:47:46 -0400

    submitting my script

---
+++ rtshw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Below is a script to add "Hello World" at the end of the file1.txt.
+
+echo "Please open file1.txt now, you will see the Hello World at the end of the file"
+
+echo "Hello World" >> $1
+
+
